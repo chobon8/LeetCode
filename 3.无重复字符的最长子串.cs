8a160cc33d52b3e0053ea8eab5121293f51d7970
dobo <zsbsdtc@gmail.com>
2020-02-23 15:09:50 +0800

    保存题目3解题代码

---
+++ 3.无重复字符的最长子串.cs
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=csharp
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+// @lc code=start
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        int[] index = new int[64];
+        int max = 0;
+        int left = 0;
+        for(int right = 0; right < s.Length; right++)
+        {
+            left = Math.Max(index[s[right]],left);
+            
+            max = Math.Max(max,right - left + 1);
+            index[s[right]] = right + 1;
+        }
+        return max;
+    }
+}
+// @lc code=end
+
