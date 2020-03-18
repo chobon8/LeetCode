895dfc7aae75d44deb09504bdb3746e76c4454d5
张双博 <shuangbo.zhang@zhiyoubao.com>
2020-03-18 12:48:21 +0800

    225题解

---
+++ 1160.拼写单词.cs
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=1160 lang=csharp
+ *
+ * [1160] 拼写单词
+ */
+
+// @lc code=start
+public class Solution {
+    public int CountCharacters(string[] words, string chars) {
+        Dictionary<char,int> dicts = new Dictionary<char,int>();
+        foreach(var c in chars)
+        {
+
+        }
+    }
+}
+// @lc code=end
+
