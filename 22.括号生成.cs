868cbce1de25787d507db387184b06b973a988bf
chobon <chobon@aliyun.com>
2020-05-08 22:57:12 +0800

    22增加广度优先搜索解法

--- 22.括号生成.cs
+++ 22.括号生成.cs
@@ -13,7 +13,7 @@ using System.Linq;
 public partial class Solution {
     public IList<string> GenerateParenthesis(int n)
     {
-        return GenerateParenthesis_DFS(n);
+        return GenerateParenthesis_BFS(n);
     }
 
     public IList<string> GenerateParenthesis_DFS(int n)
@@ -40,12 +40,53 @@ public partial class Solution {
             str = str.Remove(str.Length - 1);
         }
 
-        if (right > 0)
+        if (right > 0 && right > left)
         {
             str = str +')';
             GenerateParenthesisDFS(left, right-1, process, str);
             str.Remove(str.Length - 1);
         }
+    }
+
+    public IList<string> GenerateParenthesis_BFS(int n)
+    {
+        IList<string> result = new List<string>();
+
+        IList<Tuple<string, int, int>> queue = new List<Tuple<string, int, int>>();
+        queue.Add(new Tuple<string, int, int>("(", n - 1, n));
+
+        while (queue.Any())
+        {
+            IList<Tuple<string, int, int>> node = new List<Tuple<string, int, int>>();
+            foreach (var tupe in queue)
+            {
+                var temp = tupe.Item1;
+                var left = tupe.Item2;
+                var right = tupe.Item3;
+                if (left > 0)
+                {
+                    temp = tupe.Item1 + "(";
+                    node.Add(new Tuple<string, int, int>(temp, left - 1, right));
+                }
+
+                if (right > 0 && right > left)
+                {
+                    temp = tupe.Item1 + ")";
+                    node.Add(new Tuple<string, int, int>(temp, left, right - 1));
+                }
+
+                if (right == 0 && left == 0) result.Add(temp);
+            }
+
+            queue = node;
+        }
+
+        return result;
+    }
+
+    public IList<string> GenerateParenthesis_DynamicProgramming(int n)
+    {
+        throw new NotImplementedException();
     }
 }
 // @lc code=end
--- LeetCode/LeetCodeTests/String/22.括号生成.cs
+++ LeetCode/LeetCodeTests/String/22.括号生成.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Xunit;
 
@@ -11,9 +12,13 @@ namespace LeetCodeTests.String
         [InlineData(3,new[]{"((()))", "(()())", "(())()", "()(())", "()()()"})]
         void GenerateParenthesis(int n, string[] assert)
         {
-            var dfsResult = _solution.GenerateParenthesis_DFS(n);
+            var dfsResult = _solution.GenerateParenthesis_DFS(n).ToArray();
+
+            var bfsResult = _solution.GenerateParenthesis_BFS(n).ToArray();
 
             Assert.Equal(dfsResult, assert);
+
+            Assert.Equal(bfsResult, assert);
         }
     }
 }
