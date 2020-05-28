740ddfd670907654239675bd012b3b192894dfe0
chobon <chobon@aliyun.com>
2020-05-28 21:32:14 +0800

    455题解和测试

---
+++ LeetCode/LeetCodeTests/Greedy/455.分发饼干.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeTests.Greedy
+{
+    public partial class GreedyTest
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3 }, new[] { 1, 1 }, 1)]
+        [InlineData(new[] { 1, 2}, new[] { 1, 2, 3 }, 2)]
+        public void FindContentChildren(int[] g, int[] s, int assert)
+        {
+            var result = _solution.FindContentChildren_Greedy(g, s);
+
+            Assert.Equal(result, assert);
+        }
+    }
+}
---
+++ LeetCode/LeetCodeTests/Greedy/GreedyTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTests.Greedy
+{
+    public partial class GreedyTest : UnitTestBase
+    {
+    }
+}
