9b687f90284bd081aae247baa8d303e3b06f5b02
chobon <chobon@aliyun.com>
2020-06-01 16:38:06 +0800

    50题解增加二分解法

--- 50.pow-x-n.cs
+++ 50.pow-x-n.cs
@@ -13,21 +13,36 @@ public partial class Solution {
 
     public double MyPow_BackTracking(double x, int n)
     {
+        if (x == 0.0d) return 0.0d;
         if (n == 0) return 1.0d;
-
-        if (n < 0) 
+        long b = n;
+        if (n < 0)
         {
-            n = -n;
-            x = 1/x;
+            b = -1 * b;
+            x = 1 / x;
         }
-        double half = MyPow_BackTracking(x, n / 2);
+        double half = MyPow_BackTracking(x, (int)(b / 2));
 
         return n % 2 == 0 ? half * half : half * half * x;
     }
 
     public double MyPow_BinarySearch(double x, int n)
     {
-        return 0;
+        if(x == 0.0d) return 0.0d;
+        long b = n;
+        double res = 1.0d;
+        if(b < 0)
+        {
+            x = 1/x;
+            b = -b;
+        }
+        while(b > 0)
+        {
+            if((b & 1) == 1) res *= x; 
+            x *= x;
+            b >>= 1;
+        }
+        return res;
     }
 }
 // @lc code=end
--- LeetCode/LeetCodeTests/BackTracking/50.pow-x-n.cs
+++ LeetCode/LeetCodeTests/BackTracking/50.pow-x-n.cs
@@ -3,20 +3,21 @@ using System.Collections.Generic;
 using System.Text;
 using Xunit;
 
-namespace LeetCodeTests.BinarySearch
+namespace LeetCodeTests.BackTracking
 {
-    public partial class BinarySearchTest
+    public partial class BackTrackingTest
     {
         [Theory]
         [InlineData(2.00000, 10, 1024.00000d)]
         [InlineData(2.10000, 3, 9.26100d)]
         [InlineData(2.00000, -2, 0.25000d)]
-        [InlineData(2.00000, int.MinValue, 0)]
+        [InlineData(2.00000, int.MinValue, 0.0d)]
+        [InlineData(-1.00000, int.MinValue, 1.0d)]
         void MyPow(double x, int n, double assert)
         {
-            var result = _solution.MyPow_BinarySearch(x, n);
+            var result = _solution.MyPow_BackTracking(x, n);
 
-            Assert.Equal(result, assert,5);
+            Assert.Equal(result, assert, 5);
         }
     }
 }
