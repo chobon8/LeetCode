f93d7d3bf0ef37b8a07579bcf6d95c152ab34d97
chobon <chobon@aliyun.com>
2020-06-01 17:24:25 +0800

    188题解和测试

---
+++ LeetCode/LeetCodeTests/DynamicProgramming/188.买卖股票的最佳时机-iv.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeTests.DynamicProgramming
+{
+    public partial class DynamicProgrammingTest
+    {
+        [Theory]
+        [InlineData(2, new[] { 2, 4, 1 }, 2)]
+        [InlineData(2, new[] { 3, 2, 6, 5, 0, 3 }, 7)]
+        public void MaxProfitIV_DP(int k, int[] prices, int assert)
+        {
+            var result = _solution.MaxProfitIV_DP(k, prices);
+
+            Assert.Equal(result, assert);
+        }
+    }
+}
