9decaaf0bfe14bad53e70451f4e06fe75686a301
chobon <chobon@aliyun.com>
2020-06-01 19:25:38 +0800

    728题解和测试

---
+++ LeetCode/LeetCodeTests/Math/728.自除数.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeTests.Math
+{
+    public partial class MathTest
+    {
+        [Theory]
+        [InlineData(1,22,new []{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22 })]
+        void SelfDividingNumbers(int left, int right, int[] assert)
+        {
+            var result = _solution.SelfDividingNumbers(left, right);
+
+            Assert.Equal(result, assert);
+        }
+    }
+}
