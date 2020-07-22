a832e50eafb2aacfcbf71a1dbdf4b9f4b9226bd5
chobon <chobon@aliyun.com>
2020-07-22 21:23:11 +0800

    [70] 爬楼梯题解和测试

---
+++ LeetCode/LeetCodeTests/DynamicProgramming/70.爬楼梯.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCodeTests.DynamicProgramming
+{
+    public partial class DynamicProgrammingTest
+    {
+        [Theory]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        [InlineData(4, 5)]
+        public void ClimbStairs(int n,int assert)
+        {
+            var result = _solution.ClimbStarirs_DP(n);
+
+            Assert.Equal(result, assert);
+        }
+    }
+}
